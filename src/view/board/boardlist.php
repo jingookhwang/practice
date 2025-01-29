10cefc65a3972f340b7cacaa6d62bf32eb806a1f
jingookhwang <gkdklsl77@gmail.com>
2025-01-29 15:03:11 +0900

    db인젝션 개선-chatgpt1

--- src/view/board/boardlist.php
+++ src/view/board/boardlist.php
@@ -9,14 +9,39 @@
                   from  article 
                  order  by id desc";   
 
-    $result = mysqli_query($connection,$sqlQuery) or die("데이터 연결 실패".mysqli_error($connection));   
+    /**
+      $statement = mysqli_prepare($connection,$sqlQuery) : mysqli_prepare 함수는 SQL 쿼리를 준비하는 데 사용됩니다
+      true / false 반환
+      
+      mysqli_stmt_execute($statement) : mysql 예문 실행 
+      int 1,0 값 반환
+      
+      $result = mysqli_stmt_get_result($statement); : 준비된 문(statement)에서 결과 집합을 가져오는 데 사용됩니다.
+      성공시 array 반환 // 실패 false 반환
 
-     
+      mysqli_num_rows($result)
+      성공시 int 1 , 0 반환
+
+      mysqli_fetch_assoc($result) 
+      성공시 array 쿼리 반환 , 실패시 false
+
+      echo htmlspecialchars($row['id'])
+      텍스트 문자열 반환. html 텍스트로 저장되기에 echo 를 붙여야하고, 값을 변형 못한다.
+    
+     */
+    $statement = mysqli_prepare($connection,$sqlQuery); //쿼리 준비 단계 //true // false 반환
+    mysqli_stmt_execute($statement);//쿼리 예문 실행 int 값으로 반환
+    
+    $result = mysqli_stmt_get_result($statement);
+    if ($result === false) {
+        die("데이터 연결 실패".mysqli_error($connection));
+    }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -26,44 +51,47 @@
 </head>
 
 <body>
-<?php include '../../../public/header&footer/header.php'; ?>
+    <?php include '../../../public/header&footer/header.php'; ?>
     <main>
-    <div style="font-size: 50px; text-align: center; margin-bottom: 20px;"><a href="..\..\..\index.php">홈으로</a></div>
+        <div style="font-size: 50px; text-align: center; margin-bottom: 20px;"><a href="..\..\..\index.php">홈으로</a>
+        </div>
         <div class="container">
             <h2>게시판 목록</h2>
-        <link rel="stylesheet" href="public/resource/table.css?v=1">
-        
-        <table class="post-table">
-            <thead>
-                <tr class="table-header">
-                    <th>게시물 제목</th>
-                    <th>작성제목</th>
-                    <th>작성내용</th>
-                    <th>작성일</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php if(mysqli_num_rows($result) > 0){
-                    while($row = mysqli_fetch_assoc($result)){ 
-                ?>
-                <tr class="table-row">
-                    <td><a href="#" class="post-link"><?php echo $row['id']; ?></a></td>
-                    <td><?php echo $row['title']; ?></td>
-                    <td><?php echo $row['body']; ?></td>
-                    <td><?php echo $row['updateDate']; ?></td>
-                </tr>
-                <?php }
-                }else{
-                    echo "데이터가 없습니다".mysqli_close($connection);
-                }?>
-                
+            <link rel="stylesheet" href="public/resource/table.css?v=1">
+
+            <table class="post-table">
+                <thead>
+                    <tr class="table-header">
+                        <th>게시물 제목</th>
+                        <th>작성제목</th>
+                        <th>작성내용</th>
+                        <th>작성일</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        if(mysqli_num_rows($result) === 0){
+                            die("연결될 로우 없음".mysqli_close($connection));
+                        }else{
+                            var_dump(mysqli_fetch_assoc($result)) ;
+                            while($row = mysqli_fetch_assoc($result)){ ?>
+                            <tr class="table-row">
+                                <td><a href="#" class="post-link"><?php echo htmlspecialchars($row['id']); ?></a></td>
+                                <td><?php echo htmlspecialchars($row['title']); ?></td>
+                                <td><?php echo htmlspecialchars($row['body']); ?></td>
+                                <td><?php echo htmlspecialchars($row['updateDate']); ?></td>
+                            </tr>
+                        <?php
+                            }
+                        }?>
 
 
-            </tbody>
-        </table>
+                </tbody>
+            </table>
         </div>
     </main>
 
-<?php include '../../../public/header&footer/footer.php'; ?>
+    <?php include '../../../public/header&footer/footer.php'; ?>
 </body>
+
 </html>
